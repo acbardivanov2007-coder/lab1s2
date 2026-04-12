7d90ef450f02c1c608e5e9261107d547d23911bb
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-04-12 18:17:44 +0700

    Update datatime.h

--- lab1/datatime.h
+++ lab1/datatime.h
@@ -3,55 +3,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-// Описание структуры
 typedef struct {
-    int year;
-    int month;
     int day;
+    int month;
+    int year;
     int hour;
     int minute;
 } datatime;
 
-// --- Конструкторы и Деструктор ---
-datatime* datatime_create_default(void);
-datatime* datatime_create(int year, int month, int day, int hour, int minute);
-datatime* datatime_create_copy(const datatime* source);
-void datatime_destroy(datatime* dt);
-
-// --- Сравнение объектов ---
-int datatime_compare(const datatime* dt1, const datatime* dt2);
-bool datatime_is_equal(const datatime* dt1, const datatime* dt2);
-bool datatime_is_not_equal(const datatime* dt1, const datatime* dt2);
-bool datatime_is_greater(const datatime* dt1, const datatime* dt2);
-bool datatime_is_less(const datatime* dt1, const datatime* dt2);
-bool datatime_is_greater_or_equal(const datatime* dt1, const datatime* dt2);
-bool datatime_is_less_or_equal(const datatime* dt1, const datatime* dt2);
-
-// --- Форматированный ввод/вывод (строки) ---
-void datatime_print(const datatime* dt);
-void datatime_print_date(const datatime* dt);
-void datatime_print_time(const datatime* dt);
-datatime* datatime_from_string(const char* str);
-
-// --- Ручной безопасный ввод с клавиатуры ---
-void datatime_input(datatime* dt);
-void datatime_input_year(datatime* dt);
-void datatime_input_month(datatime* dt);
-void datatime_input_day(datatime* dt);
-
-// --- Изменение данных (++, --, защищенные setters) ---
-void datatime_increment(datatime* dt); 
-void datatime_decrement(datatime* dt); 
-
-void datatime_set_year(datatime* dt, int year);
-void datatime_set_month(datatime* dt, int month);
-void datatime_set_day(datatime* dt, int day);
-void datatime_set_hour(datatime* dt, int hour);
-void datatime_set_minute(datatime* dt, int minute);
-
-// --- Нахождение временных интервалов ---
-void datatime_difference(const datatime* dt1, const datatime* dt2);
+datatime* datatime_create(int d, int m, int y, int h, int min);
+void datatime_destroy(datatime **dt);
+void datatime_print(const datatime *dt);
+int datatime_compare(const datatime *dt1, const datatime *dt2);
+void datatime_min_increment(datatime *dt);
+void datatime_hour_increment(datatime *dt);
+void datatime_day_increment(datatime *dt);
+void datatime_month_increment(datatime *dt);
+void datatime_year_set(datatime *dt, int y);
+void datatime_month_set(datatime *dt, int m);
+void datatime_day_set(datatime *dt, int d);
+void datatime_hour_set(datatime *dt, int h);
+void datatime_min_set(datatime *dt, int min);
+void datatime_min_decrement(datatime *dt);
+void datatime_hour_decrement(datatime *dt);
+void datatime_day_decrement(datatime *dt);
+void datatime_month_decrement(datatime *dt);
+void datatime_year_decrement(datatime *dt);
+char* datatime_to_string(const datatime *dt);
+datatime* datatime_from_string(const char *str);
+void datatime_print_diff(const datatime *dt1, const datatime *dt2);
+unsigned long long datatime_to_minutes(const datatime *dt);
+datatime* datatime_from_minutes(unsigned long long total_minutes);
+unsigned long long datatime_diff_in_seconds_from_unix(const datatime *dt);
+datatime* create_empty_datatime();
+void copy_datatime(datatime *a, const datatime *b);
 
 #endif // DATATIME_H
