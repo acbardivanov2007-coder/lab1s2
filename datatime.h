4171ac72120bf23a2930d2967f7bdf739428fe7b
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2026-04-12 16:04:09 +0700

    Update datatime.h

--- datatime.h
+++ datatime.h
@@ -3,8 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// Описание структуры с использованием typedef
+// Описание структуры
 typedef struct {
     int year;
     int month;
@@ -13,28 +14,39 @@ typedef struct {
     int minute;
 } datatime;
 
-// --- "Конструкторы" (возвращают указатель на выделенную память) ---
-// Конструктор по умолчанию (установит базовую дату, например 01.01.1970)
+// --- Конструкторы и деструктор ---
 datatime* datatime_create_default(void);
-
-// Конструктор с параметрами
 datatime* datatime_create(int year, int month, int day, int hour, int minute);
-
-// Конструктор копирования
 datatime* datatime_create_copy(const datatime* source);
-
-// --- "Деструктор" ---
-// Освобождает динамическую память
 void datatime_destroy(datatime* dt);
 
-// --- Методы варианта 3 ---
-// Форматированный строковый ввод (создает объект из строки "YYYY-MM-DD HH:MM")
+// --- Сравнение объектов (==, !=, >, <, >=, <=) ---
+// Возвращает <0 если dt1 < dt2, 0 если равны, >0 если dt1 > dt2
+int datatime_compare(const datatime* dt1, const datatime* dt2);
+bool datatime_is_equal(const datatime* dt1, const datatime* dt2);       // ==
+bool datatime_is_not_equal(const datatime* dt1, const datatime* dt2);   // !=
+bool datatime_is_greater(const datatime* dt1, const datatime* dt2);     // >
+bool datatime_is_less(const datatime* dt1, const datatime* dt2);        // <
+bool datatime_is_greater_or_equal(const datatime* dt1, const datatime* dt2); // >=
+bool datatime_is_less_or_equal(const datatime* dt1, const datatime* dt2);    // <=
+
+// --- Ввод/Вывод ---
+void datatime_print(const datatime* dt);
+void datatime_print_date(const datatime* dt); // Вывод только даты
+void datatime_print_time(const datatime* dt); // Вывод только времени
 datatime* datatime_from_string(const char* str);
 
-// Форматированный строковый вывод
-void datatime_print(const datatime* dt);
+// --- Изменение данных (++, --, setters) ---
+void datatime_increment(datatime* dt); // ++ (добавляет 1 минуту)
+void datatime_decrement(datatime* dt); // -- (отнимает 1 минуту)
 
-// Нахождение временного интервала (dt2 - dt1)
+void datatime_set_year(datatime* dt, int year);
+void datatime_set_month(datatime* dt, int month);
+void datatime_set_day(datatime* dt, int day);
+void datatime_set_hour(datatime* dt, int hour);
+void datatime_set_minute(datatime* dt, int minute);
+
+// --- Нахождение интервалов ---
 void datatime_difference(const datatime* dt1, const datatime* dt2);
 
 #endif // DATATIME_H
